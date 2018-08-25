d90187a22030f751bad6b4d9c54c87c4afbf5933
yusha1439 <yushalee10@gmail.com>
2018-08-25 11:27:17 -0500

    Update api.js

--- libs/api.js
+++ libs/api.js
@@ -1,32 +1,53 @@
 var redis = require('redis');
 var async = require('async');
-
+var express = require('express');
 var stats = require('./stats.js');
+var compress = require('compression');
+
+module.exports = function(logger, portalConfig, poolConfigs){
 
 
-const loggerFactory = require('./logger.js');
-
-const logger = loggerFactory.getLogger('Api', 'system');
-
-
-module.exports = function(portalConfig, poolConfigs){
-
-
+    var app = express();
     var _this = this;
 
-    var portalStats = this.stats = new stats(portalConfig, poolConfigs);
+    var portalStats = this.stats = new stats(logger, portalConfig, poolConfigs);
 
     this.liveStatConnections = {};
 
     this.handleApiRequest = function(req, res, next){
         switch(req.params.method){
             case 'stats':
-                res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(portalStats.statsString);
                 return;
+            // hashgoal addition for better block stats
+            case 'getblocksstats':
+                portalStats.getBlocksStats(function (data) {
+                    res.end(JSON.stringify(data));
+                });
+                return;
             case 'pool_stats':
-                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                    'Cache-Control': 'max-age=20',
+                    'Connection': 'keep-alive'
+                });
                 res.end(JSON.stringify(portalStats.statPoolHistory));
+                return;
+            case 'worker_stats':
+                res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                    'Cache-Control': 'max-age=20',
+                    'Connection': 'keep-alive'
+                });
+                res.end(JSON.stringify(portalStats.statWorkerHistory));
+                return;
+                case 'algo_stats':
+                res.writeHead(200, {
+                    'Content-Type': 'text/html',
+                    'Cache-Control': 'max-age=20',
+                    'Connection': 'keep-alive'
+                });
+                res.end(JSON.stringify(portalStats.statAlgoHistory));
                 return;
             case 'live_stats':
                 res.writeHead(200, {
